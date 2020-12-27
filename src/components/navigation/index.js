626731e11efc21af15d12eeab0b1e4a294afd1e6
Fedex832 <philippe.mariel832@gmail.com>
2020-12-27 18:51:24 +0100

    feat(api)

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Axios from 'axios';
+
 import {
   Navbar,
   Form,
@@ -6,14 +8,36 @@ import {
   Button,
 } from 'react-bootstrap';
 
-const Navigation = () => (
-  <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Event Search</Navbar.Brand>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
-    </Form>
-  </Navbar>
-);
+export default class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      inputValue: '',
+    };
+    this.sendSearch = this.sendSearch.bind(this);
+  }
 
-export default Navigation;
+  Search() {
+    const { inputValue } = this.state;
+    Axios.get(`https://opendata.paris.fr/api/datasets/1.0/search/?q=${inputValue}`)
+      .then((res) => {
+        console.log(res.data.datasets);
+        this.setState({ data: res.data.datasets });
+      });
+  }
+
+  render() {
+    const { data } = this.state;
+    console.log(data);
+    return (
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Event Search</Navbar.Brand>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" onChange={(e) => this.setState({ data, inputValue: e.target.value })} />
+          <Button variant="outline-success" onClick={this.Search}>Search</Button>
+        </Form>
+      </Navbar>
+    );
+  }
+}
--- src/components/result/index.js
+++ src/components/result/index.js
@@ -1,43 +1,30 @@
 import React from 'react';
-import Axios from 'axios';
-
-const data = [
-  {
-    id: '1',
-    img: 'https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png',
-    title: 'Carte 1',
-    text: 'TEXTTEXTEXTEqedrfgqdfgqedrfgqdsrgqsrgqdsrgqsdrgezrgqesdr',
-    link: 'index.js',
-  },
-  {
-    id: '1',
-    img: 'https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png',
-    title: 'Carte 2',
-    text: 'TEXTTEXTEXTEqedrfgqdf',
-    link: 'index.js',
-  },
-];
 
 export default class Result extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
-  }
-
-  componentDidMount() {
-    Axios.get('https://opendata.paris.fr/api/records/1.0/search/?dataset=que-faire-a-paris-&q=&facet=category&facet=tags&facet=address_name&facet=address_zipcode&facet=address_city&facet=pmr&facet=blind&facet=deaf&facet=access_type&facet=price_type')
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
+    this.state = {
+      data: [],
+    };
   }
 
   render() {
+    const { data } = this.state;
+    console.log(data);
+    if (data.length === 0) {
+      return (
+        <div>
+          no data
+        </div>
+      );
+    }
     return (
-      data.map((element) => (
+      data.datasets.map((element) => (
         <div className="card">
-          <img src={element.img} className="card-img-top" alt="blah" />
+          <img src={element} className="card-img-top" alt="blah" />
           <div className="card-body">
             <h5 className="card-title">{element.title}</h5>
-            <p className="card-text">{element.text}</p>
+            <p className="card-text">{element.description}</p>
             <a href="{element.link}" className="btn btn-primary">Go somewhere</a>
           </div>
         </div>
